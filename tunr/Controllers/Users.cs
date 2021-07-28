a658dc85f1ca2ab31f9125ad2fd3a44236d1a90b
Cesar Sanchez <cesar.s@vendorpanel.com>
2021-07-28 10:27:10 +1000

    first commit

---
+++ tunr/Controllers/Users.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace tunr.Controllers
+{
+    public class UsersController : Controller
+    {
+        public IActionResult New()
+        {
+            return View();
+        }
+
+        public IActionResult Update()
+        {
+            return View();
+        }
+
+        public IActionResult Delete()
+        {
+            return View();
+        }
+    }
+}
