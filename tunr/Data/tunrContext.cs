dd87b2ce91842ea6b28ea3fa19dae24f12b89eb5
Cesar Sanchez <cesar.s@vendorpanel.com>
2021-07-30 11:34:32 +1000

    tunr dbcontext

---
+++ tunr/Data/tunrContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using tunr.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace tunr.Data
+{
+    public class tunrContext : DbContext
+    {
+        public tunrContext(DbContextOptions<tunrContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Mixtape> Mixtapes { get; set; }
+
+        // Changing table names to singular
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Mixtape>().ToTable("Mixtape");
+            modelBuilder.Entity<User>().ToTable("User");
+        }
+    }
+}
