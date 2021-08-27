a112b58cff6cc363e9dc477a33d9a9f88ee690dd
Cesar Sanchez <cesar.s@vendorpanel.com>
2021-08-27 15:39:28 +1000

    fix: added db context to users

--- tunr/Controllers/Users.cs
+++ tunr/Controllers/Users.cs
@@ -4,18 +4,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+using Microsoft.EntityFrameworkCore;
+using tunr.Data;
+using tunr.Models;
+
 namespace tunr.Controllers
 {
-    public class UsersController : Controller
+    public class Users : Controller
     {
+        private readonly tunrContext _context;
+        public Users(tunrContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult New()
         {
             return View();
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            return View(await _context.Users.ToListAsync());
         }
 
     }
